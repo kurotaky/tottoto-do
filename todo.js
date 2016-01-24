cda30dc93e392b3a1ce26d39f484f22d6384e197
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-01-25 01:55:45 +0900

    create components

--- todo.js
+++ todo.js
@@ -5,7 +5,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Button from "react-bootstrap/lib/Button";
 
+var TodoApp = React.createClass({
+  render: function() {
+    return (
+      <div className="TodoList">
+        <h1>Tottoto Do</h1>
+        <TodoForm onCommentSubmit={this.handleCommentSubmit} />
+        <TodoList /> 
+      </div>
+    );
+  }
+});
+
 var TodoList = React.createClass({
+  render: function() {
+    return (
+        <ul className="TodoList">
+          <li>布団を干す</li>
+          <li>牛乳を買う</li>
+        </ul>
+    );
+  }
+});
+
+
+var TodoForm = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
     var text = ReactDOM.findDOMNode(this.refs.text).value.trim();
@@ -17,17 +41,14 @@ var TodoList = React.createClass({
   },
   render: function() {
     return (
-      <div className="TodoList">
-        <h1>Tottoto Do</h1>
         <form className="TodoForm" onSubmit={this.handleSubmit}>
           <input type="text" placeholder="やるぞ!!!" ref="text"/>
           <Button type="submit" bsStyle="info">
             hogehoge
           </Button>
         </form>
-      </div>
     );
   }
 });
 
-ReactDOM.render(<TodoList />, document.getElementById('todo'));
+ReactDOM.render(<TodoApp />, document.getElementById('todo'));
